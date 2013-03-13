48b71a978e0a1e7f35e536b5fc7e6ba8f97d5cd2
Ryan Graham <rgraham@fortinet.com>
2013-03-13 14:08:38 -0700

    Basic tests

--- test/test_lazy_model_const.rb
+++ test/test_lazy_model_const.rb
@@ -1,7 +1,23 @@
 require 'helper'
 
+$called = 0
+class SomeModel < OpenStruct
+  extend LazyModelConst
+  lazy_model_const :IMPORTANT_INSTANCE, name: "imp", desc: "Really really important"
+  def self.find_or_create_by_name(attrs = {})
+    $called += 1
+    new(attrs)
+  end
+end
+
 class TestLazyModelConst < MiniTest::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_lazy_model_const
+    assert_equal $called, 0
+    instance = SomeModel.IMPORTANT_INSTANCE
+    assert_equal $called, 1
+    assert_equal instance, SomeModel.IMPORTANT_INSTANCE
+    assert_equal $called, 1
+    assert_equal instance, SomeModel.IMPORTANT_INSTANCE
+    assert_equal $called, 1
   end
 end
